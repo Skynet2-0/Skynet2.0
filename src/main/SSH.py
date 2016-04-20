00f209517d3819235e91dc226ec6cd607ee76085
ncbakker <ncb21992@hotmail.com>
2016-04-20 11:48:30 +0200

    checking out master

--- src/main/SSH.py
+++ src/main/SSH.py
@@ -3,14 +3,21 @@ Created on Apr 19, 2016
 
 @author: Niels
 '''
+from pip._vendor.distlib._backport.tarfile import pwd
 
-class MyClass(object):
+class SSH(object):
     '''
-    classdocs
+    This class enables the execution of SSH commands on a child server
     '''
 
-    def __init__(self, params):
+    def __init__(self, ip, username, pwd):
         '''
         Constructor
         '''
+        self.ip = ip
+        self.username = username
+        self.pwd = pwd
         
+        
+    def run(self, command):
+        pass

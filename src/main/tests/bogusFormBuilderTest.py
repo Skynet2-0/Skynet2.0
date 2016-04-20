ad5393b4d9fef5a45a7412a07cff09130c0adf32
ncbakker <ncb21992@hotmail.com>
2016-04-20 11:00:28 +0200

    removed temporary files, added .gitignore

--- src/main/tests/bogusFormBuilderTest.py
+++ src/main/tests/bogusFormBuilderTest.py
@@ -8,8 +8,6 @@ from main.bogusFormBuilder import bogusFormBuilder
 
 
 class Test(unittest.TestCase):
-
-    
         
     def setUp(self):
         self.that = bogusFormBuilder()
@@ -17,6 +15,7 @@ class Test(unittest.TestCase):
     def testPassword(self):
         pwd = self.that.getPassword()
         self.assertTrue(len(pwd)>=15, "an insecure password was created")
+        self.assertEqual(pwd, self.that.getPassword(), "memoization of password failed")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']

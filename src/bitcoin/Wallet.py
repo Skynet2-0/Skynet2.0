54782afe21f2132ce00e0d22e5dedae43e325e8d
Stefan Boodt <S.A.Boodt@student.tudelft.nl>
2016-04-20 16:49:23 +0200

    Started with an interface for the wallet and it's testing class.

---
+++ src/bitcoin/Wallet.py
@@ -0,0 +1,23 @@
+"""
+Created on April 20, 2016
+
+@author: Stefan
+"""
+class Wallet(object):
+    """
+    This class enables bitcoin transactions.
+    """
+    
+    def __init__(self):
+        """Initialize the bitcoin wallet"""
+        pass
+    
+    def getSaldo(self):
+        pass
+    
+    
+    def getAddress(self):
+        pass
+    
+    def transfer(self, amount, recipient):
+        pass
---
+++ src/bitcoin/tests/WalletTest.py
@@ -0,0 +1,28 @@
+'''
+Created on April 20, 2016
+
+@author: stefanboodt
+'''
+import unittest
+from bitcoin.Wallet import *
+
+class Test(unittest.TestCase):
+    """This class tests the wallet class"""
+
+    def setUp(self):
+        """Does some set up."""
+        self.SUT = Wallet()
+
+
+    def tearDown(self):
+        'Does some clean up'
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()

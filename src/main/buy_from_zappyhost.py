1bd9588abf45f0d2e35cd811a57f398e1dea77ef
Rob van den Berg <R.S.vandenberg-1@student.tudelft.nl>
2016-04-22 15:29:09 +0200

    Added exception handling to the code that orders from zappyhost

--- src/main/buy_from_zappyhost.py
+++ src/main/buy_from_zappyhost.py
@@ -28,66 +28,71 @@ def clickRandomSelectElement(fieldId):
     option = options[num]
     option.click()
 
-
-driver = webdriver.Remote(
-   command_executor='http://127.0.0.1:4444/wd/hub',
-   desired_capabilities=DesiredCapabilities.FIREFOX)
-
-driver.get("https://billing.zappiehost.com/cart.php?a=confproduct&i=0")
-
-#Click the to cart button for the cheapest VPS
-driver.find_element_by_css_selector('.cartbutton.ui-button.ui-widget.ui-state-default.ui-corner-all').click()
-
-#Click the continue button
-driver.find_element_by_css_selector('.cartbutton.green.ui-button.ui-widget.ui-state-default.ui-corner-all').click()
-
-
-#Click the pay by bitcoin button
-driver.find_element_by_css_selector("input[type='radio'][value='bitpay']").click()
-
-
-#driver.find_element_by_css_selector("input[name='firstname']").send_keys(getFormValue('firstname'))
-
-generator = BogusFormBuilder()
-
-
-fillInElement('firstname', generator.getFirstName())
-fillInElement('lastname', generator.getSurname())
-fillInElement('email', generator.getEmail())
-fillInElement('address1', generator.getRAString(randint(8, 15)) + ' ' + generator.getRNString(randint(1, 2)))
-fillInElement('city', generator.getCity())
-fillInElement('postcode', generator.getZipcode())
-
-clickRandomSelectElement('country')
-
-select = Select(driver.find_element_by_id('country'))
-selected_text = select.first_selected_option.text;
-
-if selected_text == 'United States' or selected_text == 'Spain' or selected_text == 'Australia' or selected_text == 'Brazil' or selected_text == 'Canada' or selected_text == 'France' or selected_text == 'Germany' or selected_text == 'India' or selected_text == 'Italy' or selected_text == 'Netherlands' or selected_text == 'New Zealand' or selected_text == 'United Kingdom':
-    # For US, Brazil, Canada, France, Germany, India, Italia, Netherlands, New Zealand and United Kingdom select state option in a select
-    clickRandomSelectElement('stateselect')
-else:
-    # For all other countries, fill in string
-    fillInElement('state', generator.getRAString(randint(6, 12)))
+try:
+    driver = webdriver.Remote(
+       command_executor='http://127.0.0.1:4444/wd/hub',
+       desired_capabilities=DesiredCapabilities.FIREFOX)
     
+    driver.get("https://billing.zappiehost.com/cart.php?a=confproduct&i=0")
+    
+    #Click the to cart button for the cheapest VPS
+    driver.find_element_by_css_selector('.cartbutton.ui-button.ui-widget.ui-state-default.ui-corner-all').click()
+    
+    #Click the continue button
+    driver.find_element_by_css_selector('.cartbutton.green.ui-button.ui-widget.ui-state-default.ui-corner-all').click()
+    
+    
+    #Click the pay by bitcoin button
+    driver.find_element_by_css_selector("input[type='radio'][value='bitpay']").click()
+    
+    
+    #driver.find_element_by_css_selector("input[name='firstname']").send_keys(getFormValue('firstname'))
+    
+    generator = BogusFormBuilder()
+    
+    
+    fillInElement('firstname', generator.getFirstName())
+    fillInElement('lastname', generator.getSurname())
+    fillInElement('email', generator.getEmail())
+    fillInElement('address1', generator.getRAString(randint(8, 15)) + ' ' + generator.getRNString(randint(1, 2)))
+    fillInElement('city', generator.getCity())
+    fillInElement('postcode', generator.getZipcode())
+    
+    clickRandomSelectElement('country')
+    
+    select = Select(driver.find_element_by_id('country'))
+    selected_text = select.first_selected_option.text;
+    
+    if selected_text == 'United States' or selected_text == 'Spain' or selected_text == 'Australia' or selected_text == 'Brazil' or selected_text == 'Canada' or selected_text == 'France' or selected_text == 'Germany' or selected_text == 'India' or selected_text == 'Italy' or selected_text == 'Netherlands' or selected_text == 'New Zealand' or selected_text == 'United Kingdom':
+        # For US, Brazil, Canada, France, Germany, India, Italia, Netherlands, New Zealand and United Kingdom select state option in a select
+        clickRandomSelectElement('stateselect')
+    else:
+        # For all other countries, fill in string
+        fillInElement('state', generator.getRAString(randint(6, 12)))
+        
+    
+    fillInElement('phonenumber', generator.getPhoneNum())
+    
+    password = generator.getPassword() # Generate a password
+    fillInElement('password', password)
+    fillInElement('password2', password)
+    
+    driver.find_element_by_css_selector("input[type='submit'][class='cartbutton green ui-button ui-widget ui-state-default ui-corner-all']").click() # Submit the form
+    
+    driver.find_element_by_css_selector("input[type='submit'][value='Pay Now']").click()
+    
+    
+    driver.implicitly_wait(10)
+    
+    bitcoinAmount = driver.find_element_by_css_selector(".ng-binding.payment__details__instruction__btc-amount").text
+    toWallet = driver.find_element_by_css_selector(".payment__details__instruction__btc-address.ng-binding").text
+    
+    print "Bitcoin amount to transfer: " + bitcoinAmount
+    
+    print "To wallet: " + toWallet
 
-fillInElement('phonenumber', generator.getPhoneNum())
-
-password = generator.getPassword() # Generate a password
-fillInElement('password', password)
-fillInElement('password2', password)
-
-driver.find_element_by_css_selector("input[type='submit'][class='cartbutton green ui-button ui-widget ui-state-default ui-corner-all']").click() # Submit the form
-
-driver.find_element_by_css_selector("input[type='submit'][value='Pay Now']").click()
-
-
-driver.implicitly_wait(10)
-
-bitcoinAmount = driver.find_element_by_css_selector(".ng-binding.payment__details__instruction__btc-amount").text
-toWallet = driver.find_element_by_css_selector(".payment__details__instruction__btc-address.ng-binding").text
-
-print "Bitcoin amount to transfer: " + bitcoinAmount
-
-print "To wallet: " + toWallet
+except Exception as e:
+    print "Could not complete the transaction because an error occurred:"
+    print e
+    #raise # Raise the exception that brought you here 
 

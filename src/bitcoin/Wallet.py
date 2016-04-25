a4f0af6880bcd0ce9916cc20f9b85eede03855c2
Stefan Boodt <S.A.Boodt@student.tudelft.nl>
2016-04-25 09:23:34 +0200

    Finished all methods.

--- src/bitcoin/Wallet.py
+++ src/bitcoin/Wallet.py
@@ -9,17 +9,29 @@ class Wallet(object):
     """This class enables bitcoin transactions."""
 
     def __init__(self):
-        """Initialize the bitcoin wallet"""
+        """Initialize the bitcoin wallet."""
         self.connection = bitcoinrpc.connect_to_local()
+        self.address = None
 
     def getSaldo(self):
-        """Checks the saldo of the wallet"""
+        """Checks the saldo of the wallet."""
         self.connection.getBalance()
 
     def getAddress(self):
         """This method gets the wallets address."""
-        pass
+        if self.hasattr(address):
+            self.address = self.connection.getAddress()
+        return self.address
 
     def transfer(self, amount, recipient):
-        """Transfers amount money from self to the recipient"""
-        self.send_to_address(recipient, amount)
+        """
+        Transfers amount money from self to the recipient.
+        Returns True iff the method to transfer is called.
+        Returns False iff the address of the recipient is invalid.
+        """
+        valid = self.validateaddress(recipient)
+        if valid.isvalid():
+            self.send_to_address(recipient, amount)
+            return True
+        else:
+            return False

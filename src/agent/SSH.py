ac80d02c3f21781777a240d593108d63956f1210
Stefan Boodt <S.A.Boodt@student.tudelft.nl>
2016-04-29 11:55:23 +0200

    Started with the SSH connection.

--- src/agent/SSH.py
+++ src/agent/SSH.py
@@ -17,8 +17,17 @@ class SSH(object):
         self.ip = ip
         self.username = username
         self.pwd = pwd
-        #self.client = SSHClient()
-        
-        
+        self.client = SSHClient()
+
+    def connect(self, sshhost, user = self.username, pwd = self.pwd):
+        '''
+        Connects this instance with the instance sshhost over SSH.
+        username is the user and pwd is the password.
+        See SSHClient.connect for more information on optional parameters
+        that can be set when using the underlying layer instead of this one.
+        '''
+        self.client.connect(sshaddress, username = user, password = pwd)
+
+
     def run(self, command):
         pass

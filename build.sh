09cb0d4ee7860e5cb1aaa26715f26dbfe49d6370
StefanBoodt <S.A.Boodt@student.tudelft.nl>
2016-05-04 12:43:29 +0200

    Removed weird lines about the submodule from the build.sh and removed the non-existing apt-get install pip3, it should be apt-get install python-pip or something of the likes.

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@
 ###########################
 git submodule add https://github.com/Tribler/tribler
 git submodule add https://github.com/prusnak/addrgen
-git submodule init --recursive /submodules
-git submodule update --recursive /submodules
+#git submodule init --recursive /submodules
+#git submodule update --recursive /submodules
 
 # update the apt-get folder.
 apt-get update
 
-apt-get install python3
-apt-get install pip3
+apt-get install -y python3
+#apt-get install -y pip3
 
 # Necessities only available through apt-get
-apt-get install firefox
+#apt-get install -y firefox
 
 #sh run.sh

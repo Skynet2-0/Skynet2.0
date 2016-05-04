c0d4ea2848add5b332028f74562cb34c8ad138d3
ncbakker <ncb21992@hotmail.com>
2016-05-04 12:18:18 +0200

    updated build log

--- build.sh
+++ build.sh
@@ -5,10 +5,6 @@
 #only works on ubuntu 14.04 and 16.04#
 ######################################
 
-#install other tribler requirements
-sudo apt-get install libav-tools libjs-excanvas libjs-mootools libx11-6 python-apsw python-cherrypy3 python-crypto python-feedparser python-leveldb python-libtorrent python-m2crypto python-netifaces python-pil python-pyasn1 python-requests python-twisted python-wxgtk2.8 python2.7 vlc python-pip
-pip install decorator
-
 #ensure wxgtk2.8 and all requirements are available for 16.04
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu vivid main"
 sudo apt-get update
@@ -16,12 +12,15 @@ sudo apt-get update
 #install libsodium13 which does not exist anymore under 14.04
 wget http://launchpadlibrarian.net/191505542/libsodium13_1.0.1-1_amd64.deb
 dpkg -i libsodium13_1.0.1-1_amd64.deb
-apt-get install
 
 #install python-cryptography which does not exist anymore under 14.04
 wget http://launchpadlibrarian.net/200479300/python-cryptography_0.8-1ubuntu2_amd64.deb
 dpkg -i python-cryptography_0.8-1ubuntu2_amd64.deb
-apt-get install
+
+
+#install other tribler requirements
+sudo apt-get install libav-tools libjs-excanvas libjs-mootools libx11-6 python-apsw python-cherrypy3 python-crypto python-feedparser python-leveldb python-libtorrent python-m2crypto python-netifaces python-pil python-pyasn1 python-requests python-twisted python-wxgtk2.8 python2.7 vlc python-pip python-decorator python-gmpy python-cryptography gconf2
+pip install decorator
 
 #install tribler itself
 wget https://github.com/Tribler/tribler/releases/download/v6.5.1/tribler_6.5.1_all.deb

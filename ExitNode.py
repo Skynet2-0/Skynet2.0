6524e35884a7661e95c99733324a38083c2b90d3
ncbakker <ncb21992@hotmail.com>
2016-05-13 12:06:36 +0200

    updated exitnode

--- ExitNode.py
+++ ExitNode.py
@@ -2,12 +2,28 @@ from Tribler.Core.SessionConfig import SessionStartupConfig
 from Tribler.Core.Session import Session
 
 class ExitNode(object):
+	'''
+	Modification of the tunnel main.py
+	Used to run an exitnode instance with multichain enabled
+	'''
+
+	__single = None	
+	
 	def __init__(self):
 		'''
 		initializes a tribler exit node on the system
 		ToDo: maybe the class should be singleton?
 		'''
+		if ExitNode.__single:
+			raise RuntimeError("ExitNode is singleton")
+		ExitNode.__single = self
 		self.start()
+		
+	def get_instance(*args, **kw):
+		if ExitNode.__single is None:
+			ExitNodel(*args, **kw)
+		return ExitNode.__single
+	get_instance = staticmethod(get_instance)
 		
 	def start(self):
 		'''
@@ -27,7 +43,7 @@ class ExitNode(object):
 		upgrader = self.session.prestart()
 		while not upgrader.is_done:
 			time.sleep(0.1)
-		self.session.start()
-		logger.info("Using port %d" % self.session.get_dispersy_port())
+		self.session.prestart()
+		#logger.info("Using port %d" % self.session.get_dispersy_port())
       
 		self.session.start()

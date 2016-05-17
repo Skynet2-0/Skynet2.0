444cff2df8e10bb0851a7f199633ec28511c9fce
ncbakker <ncb21992@hotmail.com>
2016-05-17 13:28:02 +0200

    fixed buildscript (again)

--- build.sh
+++ build.sh
@@ -11,13 +11,15 @@ sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu vivid main"
 # update the apt-get folder.
 sudo apt-get update
 
+sudo apt install python-pip
+
 #install libsodium13 which does not exist anymore under 14.04
-rm libsodium13_1.0.1-1_amd64.deb
+
 wget http://launchpadlibrarian.net/191505542/libsodium13_1.0.1-1_amd64.deb
 dpkg -i libsodium13_1.0.1-1_amd64.deb
 
 #install python-cryptography which does not exist anymore under 14.04
-rm python-cryptography_0.8-1ubuntu2_amd64.deb
+
 wget http://launchpadlibrarian.net/200479300/python-cryptography_0.8-1ubuntu2_amd64.deb
 dpkg -i python-cryptography_0.8-1ubuntu2_amd64.deb
 
@@ -26,21 +28,26 @@ sudo apt-get install -y libav-tools libjs-excanvas libjs-mootools libx11-6 pytho
 pip install decorator==4.0.9
 
 #install tribler itself
-rm tribler_6.5.1_all.deb
-wget https://github.com/Tribler/tribler/releases/download/v6.5.1/tribler_6.5.1_all.deb
-dpkg -i tribler_6.5.1_all.deb
-apt-get install -y -f
 
+#wget https://github.com/Tribler/tribler/releases/download/v6.5.2/tribler_6.5.2_all.deb
+#dpkg -i tribler_6.5.2_all.deb
+#apt-get install -y -f
+
+pip install git+https://github.com/Tribler/tribler
+
+rm python-cryptography_0.8-1ubuntu2_amd64.deb
+#rm tribler_6.5.2_all.deb
+rm libsodium13_1.0.1-1_amd64.deb
 
 ###########################
 #initialize the submodules#
 ###########################
 git submodule add https://github.com/Tribler/tribler
 git submodule add https://github.com/prusnak/addrgen
-#git submodule init --recursive
+git submodule init --recursive
 #git submodule update --recursive
 
-apt-get install -y python3
+#apt-get install -y python3
 #apt-get install -y pip3
 
 # Necessities only available through apt-get
--- getaddr.sh
+++ getaddr.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-python ./submodules/addrgen-master/addrgen.py
+python ./addrgen2/addrgen.py

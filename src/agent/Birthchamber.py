1753a4ad36a64377da8c194ead9eb22d2af50fc3
Niels Bakker <niels@connectsb.nl>
2016-05-17 22:14:15 +0200

    update birthchamber

--- src/agent/Birthchamber.py
+++ src/agent/Birthchamber.py
@@ -8,30 +8,33 @@ from src.ssh.install import Installer
 from src.ssh.starter import Starter
 
 class Birthchamber(object):
-	def __init__(self):
-		pass
+	'''
+	This class is responsible for creating children of the agent
+	'''
+    def __init__(self):
+        pass
 
-	def getChild(self):
-		#do a startup message
-		print("Starting up a child server")
+    def getChild(self):
+        #do a startup message
+        print("Starting up a child server")
 
-		#buy a server
-		zhb = ZappiehostBuyer()
-		result = zhb.buy()
+        #buy a server
+        zhb = ZappiehostBuyer()
+        result = zhb.buy()
 
-		if result == True:
-			print("VPS BOUGHT! Details:")
-			print("Zappiehost email: " + zhb.getEmail())
-			print("Zappiehost password: " + zhb.getPassword())
-			print("SSH IP: " + zhb.getIP())
-			print("SSH Username: " + zhb.getSSHUsername())
-			print("SSH Password: " + zhb.getSSHPassword())
-		else:
-			print("Failed to buy VPS from Zappiehost...")
-			#maybe do an alternative vps?
-			
-		#run installation on vps
-		Installer(zhb.getIP(),zhb.getSSHUsername(),zhb.getSSHPassword(), 21)
+        if result == True:
+            print("VPS BOUGHT! Details:")
+            print("Zappiehost email: " + zhb.getEmail())
+            print("Zappiehost password: " + zhb.getPassword())
+            print("SSH IP: " + zhb.getIP())
+            print("SSH Username: " + zhb.getSSHUsername())
+            print("SSH Password: " + zhb.getSSHPassword())
+        else:
+            print("Failed to buy VPS from Zappiehost...")
+            #maybe do an alternative vps?
+            
+        #run installation on vps
+        Installer(zhb.getIP(),zhb.getSSHUsername(),zhb.getSSHPassword(), 21)
 
-		#start the core program on child
-		Starter(zhb.getIP(),zhb.getSSHUsername(),zhb.getSSHPassword(), 21)
+        #start the core program on child
+        Starter(zhb.getIP(),zhb.getSSHUsername(),zhb.getSSHPassword(), 21)

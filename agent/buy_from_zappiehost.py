5588886f12410859b4e3622862c7f2d46885bf2d
StefanBoodt <S.A.Boodt@student.tudelft.nl>
2016-05-18 14:20:27 +0200

    Removed the seemingly unnecessary imports from the file.
    Should fix #24.

--- agent/buy_from_zappiehost.py
+++ agent/buy_from_zappiehost.py
@@ -1,17 +1,4 @@
-from random import randint
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-
-from BogusFormBuilder import BogusFormBuilder
-
-from VPSBuyer import VPSBuyer
 from ZappiehostBuyer import ZappiehostBuyer
-
-
 
 
 # The code here will actually buy a VPS from Zappiehost

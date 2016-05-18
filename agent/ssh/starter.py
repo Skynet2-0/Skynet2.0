5c9f331704eeac6adfd6ac9a90bdbbd86d9ffec6
StefanBoodt <S.A.Boodt@student.tudelft.nl>
2016-05-18 13:28:08 +0200

    Started with refactoring of the python classes.

--- agent/ssh/starter.py
+++ agent/ssh/starter.py
@@ -4,21 +4,22 @@ This class can start a child once it has been installed unto
 
 from SSH import SSH
 
+
 class Starter:
     '''
     This class is responsable for sending the startup commands to
     the child server. It uses the SSH class to send them.
     '''
-    
-    def __init__(self, hostip, user, password, port = None):
+
+    def __init__(self, hostip, user, password, port=None):
         '''
         Constructs the Installer.
+
         hostip is the ip address of the host.
         user is the username of the user (Usually it will be host).
         password is the password.
         port is the port to connect to.
         '''
-        
         self.ssh = SSH(hostip, user, password, port)
 
     def start(self):

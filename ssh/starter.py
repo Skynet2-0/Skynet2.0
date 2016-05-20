a90a4a6f6f7cee7d6ff1e959ea6008d42f35dcb8
StefanBoodt <S.A.Boodt@student.tudelft.nl>
2016-05-20 11:49:10 +0200

    Removed the non existing src folder from some paths.
    Fixes #28.
    
    Although run.bat is outdated I still removed the src folder from it and made it do approximately the same thing as run.sh.

--- ssh/starter.py
+++ ssh/starter.py
@@ -13,7 +13,7 @@ class Starter:
 
     def __init__(self, hostip, user, password, port=None):
         '''
-        Constructs the Installer.
+        Constructs the Starter which starts the program.
 
         hostip is the ip address of the host.
         user is the username of the user (Usually it will be host).
@@ -23,5 +23,8 @@ class Starter:
         self.ssh = SSH(hostip, user, password, port)
 
     def start(self):
-        (_, out0, err0) = self.ssh.run('PYTHONPATH="Skynet2.0"python Skynet2.0/src/agent/agentCore.py')
+        """
+        Starts the program.
+        """
+        (_, out0, err0) = self.ssh.run('PYTHONPATH="Skynet2.0" python Skynet2.0/agent/agentCore.py')
         #self._checkStreams(out0, err0, 'apt update failed', 'apt updated.')

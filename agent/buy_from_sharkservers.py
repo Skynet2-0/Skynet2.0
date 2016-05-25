52cdeeae3fdb8674d189dbf7da4a90eb8c0f8bd2
Rob van den Berg <R.S.vandenberg-1@student.tudelft.nl>
2016-05-25 16:52:58 +0200

    Added SharkserversBuyer

---
+++ agent/buy_from_sharkservers.py
@@ -0,0 +1,16 @@
+from SharkserversBuyer import SharkserversBuyer
+
+
+# The code here will actually buy a VPS from Zappiehost
+zhb = SharkserversBuyer()
+result = zhb.buy()
+
+if result == True:
+    print("VPS BOUGHT! Details:")
+    print("Zappiehost email: " + zhb.getEmail())
+    print("Zappiehost password: " + zhb.getPassword())
+    print("SSH IP: " + zhb.getIP())
+    print("SSH Username: " + zhb.getSSHUsername())
+    print("SSH Password: " + zhb.getSSHPassword())
+else:
+    print("Failed to buy VPS from Zappiehost...")

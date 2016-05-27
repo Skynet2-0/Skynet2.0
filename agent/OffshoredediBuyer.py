d2f2153942494e9020adf0dfb13c9d954c5f7d73
StefanBoodt <S.A.Boodt@student.tudelft.nl>
2016-05-27 12:41:20 +0200

    Added some tests for the OffshoreDediBuyer.
    They are all passing. Now the waiting can be done by having a simple wait method and it is still tested.

--- agent/OffshoredediBuyer.py
+++ agent/OffshoredediBuyer.py
@@ -195,24 +195,27 @@ class OffshoredediBuyer(VPSBuyer):
         except Exception as e:
             return True
 
-    def _wait_for_transaction(self, wait_test, number_of_tries, sleeptime=1):
+    def _wait_for_transaction(self, wait_test, number_of_tries, sleeptime=1, log=True):
         """
         Waits for the transaction to be accepted.
 
         wait_test -- The test to perform to check whether to keep waiting. The
-        footprint of the test is that it accepts nothing and returns a boolean.
+        footprint of the test is that it accepts only self and returns a boolean.
         It should return False if we still need to wait.
         number_of_tries -- The number of times to try if the connection
         is already working.
         sleeptime -- The time in seconds to wait between two tries. (Default is 1)
+        log -- Whether to print the number of tries left to the screen. (Default is True)
 
         returns True if stopped because the wait ended and False on timeout.
         """
+        tries_left = number_of_tries
         done = False
         while(not done and tries_left > 0):
             tries_left = tries_left - 1
-            done = wait_test()
+            done = wait_test(self)
             if not done:
-                print("Tries left: %i" % tries_left)
-                sleep(sleeptime)
+                if log:
+                    print("Tries left: %i" % tries_left)
+                time.sleep(sleeptime)
         return done;
--- tests/OffshoredediBuyerTest.py
+++ tests/OffshoredediBuyerTest.py
@@ -7,6 +7,23 @@ import unittest
 from VPSBuyerTest import VPSBuyerTest
 from agent.OffshoredediBuyer import OffshoredediBuyer
 
+class CNTR(object):
+    """Simple counter."""
+    counter = 0
+
+    @classmethod
+    def getCounter(cls):
+        return cls.counter
+
+    @classmethod
+    def decrement(cls):
+        cls.counter = cls.counter - 1
+
+    @classmethod
+    def setCounter(cls, cntr):
+        assert type(cls.counter) == int
+        cls.counter = cntr
+
 
 class OffshoredediBuyerTest(VPSBuyerTest):
     """
@@ -14,6 +31,7 @@ class OffshoredediBuyerTest(VPSBuyerTest):
 
     Only non buying methods are tested.
     """
+
 
     def setUp(self):
         VPSBuyerTest.setUp(self)
@@ -26,6 +44,34 @@ class OffshoredediBuyerTest(VPSBuyerTest):
 
     def testSSHName(self):
         self.assertEqual(self.sshuser, self.buyer.getSSHUsername())
+
+    def testWaitForTransactionTrue(self):
+        def always_true(self):
+            return True
+        self.assertTrue(self.buyer._wait_for_transaction(always_true, 10, 1, False))
+
+    def testWaitForTransactionFalse(self):
+        def always_false(self):
+            return False
+        self.assertFalse(self.buyer._wait_for_transaction(
+        always_false, 5, 1, False))
+
+    def testWaitForTransactionThreeTurnsFinishes(self):
+        CNTR.setCounter(3)
+        def third_time(self):
+            import tests.OffshoredediBuyerTest
+            CNTR.decrement()
+            return CNTR.getCounter() == 0
+        self.assertTrue(self.buyer._wait_for_transaction(third_time, 5))
+
+    def testWaitForTransactionThreeTurnsActuallyThree(self):
+        CNTR.setCounter(3)
+        def third_time(self):
+            import tests.OffshoredediBuyerTest
+            CNTR.decrement()
+            return CNTR.getCounter() == 0
+        self.buyer._wait_for_transaction(third_time, 5, 1, False)
+        self.assertEqual(0, CNTR.counter)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']

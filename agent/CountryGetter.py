b253d40944a5c2a36a8f0ee4b3d1133d104e797a
Rob <R.S.vandenberg-1@student.tudelft.nl>
2016-06-03 15:51:51 +0200

    Added functionality for checking the country a VPS is in

---
+++ agent/CountryGetter.py
@@ -0,0 +1,17 @@
+from geoip import geolite2
+import ipgetter
+
+
+class CountryGetter(object):
+    '''
+    This class enables you to get your own country code by your IP address
+    '''
+
+    @staticmethod
+    def get_country():
+        '''
+        Returns country code of the IP address that belongs to the machine this method is called on
+        '''
+        myip = ipgetter.myip()
+        match = geolite2.lookup(myip)
+        return match.country

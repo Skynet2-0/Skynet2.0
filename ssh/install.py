941aeb4a391c235c8672141ef33a479291d2cee3
Stefan Boodt <S.A.Boodt@student.tudelft.nl>
2016-06-03 15:01:13 +0200

    Initial version of starter and install supporting docker.
    As there is no vps available to test this over it is for now remaining as initial.
    
    An important thing to check remains the checking if ssh can access the container of docker. If it cannot a workaround is needed such as adding docker img before each command, where img is the image name.
    
    Additionally the install should still fail as the docker image is not loaded there yet, as the decision how to load it still needs to be made.

--- ssh/install.py
+++ ssh/install.py
@@ -5,9 +5,42 @@ Created on May 2, 2016.
 """
 from SSH import SSH
 import time
+from abc import *
 
 
-class Installer(object):
+class AbstractInstaller(object):
+    """
+    Abstract class that facilitates installation of child servers.
+
+    The SSH class is used to send the commands.
+    """
+    __metaclass__ = ABCMeta
+
+    def __init__(self, hostip, user, password, port=None, use_log=None):
+        """
+        Constructs the Installer.
+
+        hostip -- the ip address of the host.
+        user -- the username of the user (Usually it will be host).
+        password -- the password.
+        port -- the port to connect to. (Default is None)
+        use_log -- True for logging the installation. (Default is None)
+        """
+        if (use_log is None):
+            self.ssh = SSH(hostip, user, password, port)
+        else:
+            self.ssh = SSH(hostip, user, password, port, use_log)
+
+    @abstractmethod
+    def install(self):
+        """Makes the class install itself through SSH."""
+        pass
+
+    def finish(self):
+        "Does some clean up."
+        self.ssh.close_connection()
+
+class Installer(AbstractInstaller):
     """
     This class is responsable for sending the install commands to the child
     server.
@@ -25,20 +58,10 @@ class Installer(object):
         port -- the port to connect to. (Default is None)
         use_log -- True for logging the installation. (Default is None)
         """
-        # self.ip = hostip
-        # self.user = user
-        # self.password = password
-        if (use_log is None):
-            self.ssh = SSH(hostip, user, password, port)
-        else:
-            self.ssh = SSH(hostip, user, password, port, use_log)
+        super(Installer, self).__init__(hostip, user, password, port, use_log)
 
     def install(self):
-        """
-        Makes the class install itself through SSH.
-
-        It also starts running the class.
-        """
+        """Makes the project install itself through SSH."""
         print('Start installing.')
         (_, out0, err0) = self.ssh.run('apt-get update')
         self.ssh._checkStreams(out0, err0, 'apt update failed', 'apt updated.')
@@ -52,6 +75,30 @@ class Installer(object):
         self.ssh._checkStreams(out0, err0, 'build failed', 'project build.')
         print('Installation finished.')
 
-    def finish(self):
-        "Does some clean up."
-        self.ssh.close_connection()
+    class DockerInstaller(AbstractInstaller):
+        """
+        This class aranges the installation through the docker class.
+        """
+
+        def __init__(self, hostip, user, password, port=None, use_log=None):
+            """
+            Constructs the Installer.
+
+            hostip -- the ip address of the host.
+            user -- the username of the user (Usually it will be host).
+            password -- the password.
+            port -- the port to connect to. (Default is None)
+            use_log -- True for logging the installation. (Default is None)
+            """
+            super(DockerInstaller, self).__init__(hostip, user, password, port, use_log)
+
+        def install(self):
+            """Makes the project """
+            (_, out, err) = self.ssh.run('apt-get install -y curl')
+            self.ssh._checkStreams(out, err, 'Installing curl failed', 'curl installed')
+            (_, out, err) = self.ssh.run('curl -fsSL https://get.docker.com/gpg | apt-key add -')
+            self.ssh._checkStreams(out, err, 'Adding docker key failed', 'Docker key added')
+            (_, out, err) = self.ssh.run('curl -fsSL https://get.docker.com/ | sh')
+            self.ssh._checkStreams(out, err, 'Installing docker failed', 'Docker installed')
+            (_, out, err) = self.ssh.run('') # Load the image.
+            self.ssh._checkStreams(out, err, 'Failed loading image', 'Docker image loaded')
--- ssh/starter.py
+++ ssh/starter.py
@@ -3,13 +3,12 @@ This class can start a child once it has been installed unto.
 """
 
 from SSH import SSH
+from abc import *
 
 
-class Starter:
-    """
-    This class is responsable for sending the startup commands to
-    the child server. It uses the SSH class to send them.
-    """
+class AbstractStarter(object):
+    """Abstract class that arranges the starting up of the server."""
+    __metaclass__ = ABCMeta
 
     def __init__(self, hostip, user, password, port=None, use_log=None):
         """
@@ -26,7 +25,57 @@ class Starter:
         else:
             self.ssh = SSH(hostip, user, password, port, use_log)
 
+    @abstractmethod
+    def start(self):
+        """ Starts the program. """
+        pass
+        #self._checkStreams(out0, err0, 'apt update failed', 'apt updated.')
+
+    def finish(self):
+        """Does some clean up for the class."""
+        self.ssh.close_connection()
+
+class Starter(AbstractStarter):
+    """
+    This class is responsable for sending the startup commands to
+    the child server. It uses the SSH class to send them.
+    """
+
+    def __init__(self, hostip, user, password, port=None, use_log=None):
+        """
+        Constructs the Starter which starts the program.
+
+        hostip -- the ip address of the host.
+        user -- the username of the user (Usually it will be host).
+        password -- the password.
+        port -- the port to connect to. (Default is None)
+        use_log -- True for logging the starting. (Default is None)
+        """
+        super(Starter, self).__init__(hostip, user, password, port, use_log)
+
     def start(self):
         """ Starts the program. """
         (_, out0, err0) = self.ssh.run('sh run.sh')
         #self._checkStreams(out0, err0, 'apt update failed', 'apt updated.')
+
+class DockerStarter(AbstractStarter):
+    """Starts up the project using docker."""
+
+    def __init__(self, hostip, user, password, port=None, use_log=None):
+        """
+        Constructs the Starter which starts the program.
+
+        hostip -- the ip address of the host.
+        user -- the username of the user (Usually it will be host).
+        password -- the password.
+        port -- the port to connect to. (Default is None)
+        use_log -- True for logging the starting. (Default is None)
+        """
+        super(DockerStarter, self).__init__(hostip, user, password, port, use_log)
+
+    def start(self):
+        """Starts the program."""
+        (_, out, err) = self.ssh.run('docker -t -i root/ubuntu:Skynet2.0 /bin/bash')
+        self.ssh._checkStreams(out, err, 'Container creation failed', 'Succesfully created container.')
+        (_, out, err) = self.ssh.run('cd Skynet2.0 && sh run.sh')
+        self.ssh._checkStreams(out, err, 'Program did not start', 'Program started.')

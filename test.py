4c9826ec0625728c8c6e9e46ee6d200b4153255c
ncbakker <ncb21992@hotmail.com>
2016-06-03 15:44:33 +0200

    modified install procedure of test to use prototype2

--- test.py
+++ test.py
@@ -21,7 +21,20 @@ print(v.IP)
 bc = Birthchamber()
 bc.vps = v
 
-bc.installChild()
+#bc.installChild()
+print('Start installing.')
+(_, out0, err0) = ssh.run('apt-get update')
+ssh._checkStreams(out0, err0, 'apt update failed', 'apt updated.')
+(_, out0, err0) = ssh.run('apt-get install -y git')
+ssh._checkStreams(out0, err0, 'git install failed', 'git installed.')
+command = 'git clone --recursive https://github.com/Skynet2-0/Skynet2.0.git'
+(_, out0, err0) = ssh.run(command)
+ssh._checkStreams(out0, err0, 'git clone failed', 'project cloned.')
+command = """cd Skynet2.0 && git checkout prototype2 && sh build.sh"""
+(_, out0, err0) = ssh.run(command)
+ssh._checkStreams(out0, err0, 'build failed', 'project build.')
+print('Installation finished.')
+
 bc.giveChildGeneticCode(DNA())
 bc.startChild()
 print("agentcore running on the server")

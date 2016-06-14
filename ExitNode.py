9dd615cededcf4ebb3b98f713e8c42953835408c
StefanBoodt <S.A.Boodt@student.tudelft.nl>
2016-06-14 12:06:56 +0000

    Took out the little bug in exitnode.py

--- ExitNode.py
+++ ExitNode.py
@@ -192,7 +192,7 @@ class Tunnel(object):
         def start_market_community():
             """This will start the market community, do not forget to start this in a seperate thread."""
             keypair = self.dispersy.crypto.generate_key(u"curve25519")
-            member = dispersy.get_member(private_key=self.dispersy.crypto.key_to_bin(keypair))
+            member = self.dispersy.get_member(private_key=self.dispersy.crypto.key_to_bin(keypair))
             cls = MarketCommunity
             self.market_community = self.dispersy.define_auto_load(cls, member, (self.session, self.settings), load=True)[0]
             if introduce_port:

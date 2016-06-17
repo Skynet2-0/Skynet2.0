9d3e848f08e5a3e36da3ef9915f62ed24fef0eb0
Rob <R.S.vandenberg-1@student.tudelft.nl>
2016-06-17 09:44:47 +0200

    Added getPrice to VPSBuyer

--- agent/VPSBuyer.py
+++ agent/VPSBuyer.py
@@ -109,9 +109,6 @@ class VPSBuyer(object):
     def getPassword(self):
         """Returns the password to log in on the VPS provider."""
         return self.password
-   
-    def getPrice(self):
-        return self.password
     
     def getPrice(self):
         return self.price
